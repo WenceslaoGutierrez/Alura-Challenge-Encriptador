8ae6d32da9b9e9b8a095c4bb0306601d2dad4cf8
WenceslaoGutierrez <wences_gv@hotmail.com>
2024-03-13 12:04:40 -0600

    Corrección de typos y encriptación
    Se arreglaron errores ortográficos y se agregaron nuevas condiciones para la encriptación.

--- js/app.js
+++ js/app.js
@@ -8,11 +8,8 @@ buttontest.addEventListener("click",encrypt);
 function encrypt(){
     /* Save message from ID message */
     const message=document.getElementById("message").value;
-    /* Check if the message is empty */
-    if(message.trim().length === 0){
-        console.log("message is empty");
-        return;
-    }
+    /* Check if the message is valid */
+    if(checkMessage(message)) return;
     /* Split string into characters */
     const splitMessage=message.split("");
     /* Variable for encrypting message */
@@ -44,6 +41,31 @@ function encrypt(){
     updateMessage(encryptedMessage);
 };
 
+function checkMessage(message){
+     /* Check if the message is empty */
+    if(message.trim().length === 0){
+        console.log("message is empty");
+        return true;
+    };
+    /* Regex with special characters, accents and uppercases */
+    const regex = /[\p{P}\p{M}\p{Mn}\p{Lu}]+/u;        
+    /* Check if the message has special characters */
+    if(regex.test(message)){
+        console.log("message has special characters")
+        return true;
+    };
+};
+
+function notValidMessage(message){
+    /* Regex with special characters, accents and uppercases */
+    const regex = /[\p{P} \p{M} \p{Mn} \p{Lu}]+/u;
+    /* Check if the message has special characters */
+    if(regex.test(message)){
+        console.log("message has special characters")
+        return true;
+    };
+}
+
 function messageNotFound(){
     const screenText=document.getElementById("messageScreen");
     document.getElementById("screen").classList.add("sideContent--image");
@@ -55,9 +77,9 @@ function messageNotFound(){
     suggest.id="suggest"
     screenText.appendChild(heading);
     screenText.appendChild(suggest);
-}
+};
 function updateMessage(message){
     document.getElementById("screen").classList.remove("sideContent--image");
     document.getElementById("heading").textContent="";
     document.getElementById("suggest").textContent=message;
-}
+};
