e1f7a03a1cd1e5c80c9fa7c529fc6ea47bc8c35f
WenceslaoGutierrez <wences_gv@hotmail.com>
2024-03-16 12:24:11 -0600

    Función de desencriptar y correcciones de estilos
    Se agregó la función de desencriptado, se corrigió código de funciones y se corrigieron estilos al mostrar los mensajes del usuario.

--- js/app.js
+++ js/app.js
@@ -1,54 +1,63 @@
 /* Message not found */
-messageNotFound();
+startingConditions();
 
 /* Identify encrypt button */
-const buttontest=document.getElementById("encrypt");
-buttontest.addEventListener("click",encrypt);
-
+const buttonEncrypt=document.getElementById("encrypt");
+const buttonDecrypt=document.getElementById("decrypt");
+buttonEncrypt.addEventListener("click",encrypt);
+buttonDecrypt.addEventListener("click",decrypt);
+/* Save message from ID message */
+const message = document.getElementById("message");
+/* Array with chars and encryption keys */
+const encryptionsChars = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"]
+];
 function encrypt() {
-    /* Save message from ID message */
-    const message = document.getElementById("message").value;
     /* Check if the message is valid */
-    if (checkMessage(message)) return;
-
-    /* Object with chars and encryptions */
-    const encryptionsChars = {
-        "a": "ai",
-        "e": "enter",
-        "i": "imes",
-        "o": "ober",
-        "u": "ufat"
-    };
-
-    /* Variable for encrypting message */
+    if (checkMessage()) return;
     let encryptedMessage = "";
-
     /* Encryption loop */
-    for (let char of message) {
+    for (let char of message.value) {
         /* Find current character in encryptionsChars */
-        const replacement = encryptionsChars[char];
+        const replacement = encryptionsChars.find(pair => pair[0] === char);
         /* If it finds a replacement, adds it to the encrypted message */
-        encryptedMessage += replacement ? replacement : char;
+        encryptedMessage += replacement ? replacement[1] : char;
     }
-
-    console.log(encryptedMessage);
-    updateMessage(encryptedMessage);
+    updateMessage(encryptedMessage,"valid");
 };
-function checkMessage(message){
+function decrypt(){
+    /* Check if the message is valid */
+    if (checkMessage()) return;
+    /* Save the original message */
+    let decryptedMessage=message.value;
+    for (let pair of encryptionsChars) {
+        const cipher = pair[1];
+        const original = pair[0];
+        decryptedMessage = decryptedMessage.replaceAll(cipher, original);
+    }
+    updateMessage(decryptedMessage,"valid");
+}
+function checkMessage(){
      /* Check if the message is empty */
-    if(message.trim().length === 0){
+    if(message.value.trim().length === 0){
         console.log("message is empty");
+        updateMessage(message,"empty");
         return true;
     };
-    /* Regex that isn't from a-z or a number */
-    const regex = /[^a-z0-9\s]+/u;        
+    /* Regex that isn't from a-z */
+    const regex = /[^a-z\s]+/u;        
     /* Check if the message has special characters */
-    if(regex.test(message)){
+    if(regex.test(message.value)){
         console.log("message has special characters")
+        updateMessage(message,"special");
         return true;
     };
 };
-function messageNotFound(){
+function startingConditions(){
     const screenText=document.getElementById("messageScreen");
     document.getElementById("screen").classList.add("sideContent--image");
     const heading=document.createElement("H1");
@@ -59,9 +68,37 @@ function messageNotFound(){
     suggest.id="suggest"
     screenText.appendChild(heading);
     screenText.appendChild(suggest);
+    updateMessage("","empty");
 };
-function updateMessage(message){
-    document.getElementById("screen").classList.remove("sideContent--image");
-    document.getElementById("heading").textContent="";
-    document.getElementById("suggest").textContent=message;
+function updateMessage(message, status) {
+    const screenElement = document.getElementById("screen");
+    const screenText=document.getElementById("messageScreen");
+    const headingElement = document.getElementById("heading");
+    const suggestElement = document.getElementById("suggest");
+    
+    switch (status) {
+        case "empty":
+            screenText.classList.remove("sideContent__texts--active");
+            screenElement.classList.remove("sideContent--block");
+            screenElement.classList.add("sideContent--image");
+            headingElement.textContent = "Ningún mensaje fue encontrado";
+            suggestElement.textContent = "Asegurate de ingresar el texto que deseas encriptar o desencriptar";
+            break;
+            case "special":
+            screenElement.classList.remove("sideContent--block");
+            screenText.classList.remove("sideContent__texts--active");
+            screenElement.classList.add("sideContent--image");
+            headingElement.textContent = "Mensaje no válido";
+            suggestElement.textContent = "Por favor asegúrate de solo utilizar letras minúsculas y sin acentos";
+            break;
+            case "valid":
+            screenElement.classList.add("sideContent--block");
+            screenText.classList.add("sideContent__texts--active");
+            screenElement.classList.remove("sideContent--image");
+            headingElement.textContent = "";
+            suggestElement.textContent = message;
+            break;
+        default:
+            break;
+    }
 };
